6e621efb7b1cc3c05967a1fca9916f07508667ff
Chandra-Mauli-Sharma <chandramaulisharma26@gmail.com>
2022-01-03 23:33:39 +0530

    Some UI fixes

--- app/src/main/java/com/example/coeus/SignUp.kt
+++ app/src/main/java/com/example/coeus/SignUp.kt
@@ -13,6 +13,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.example.coeus.model.UserEntity
 import com.example.coeus.viewmodels.UserViewModel
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class SignUp : Fragment() {
@@ -24,14 +25,20 @@ class SignUp : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_sign_up, container, false)
         mUserViewModel = ViewModelProvider(this)[UserViewModel::class.java]
-        view.findViewById<Button>(R.id.SignIn).setOnClickListener{
+
+        view.findViewById<Button>(R.id.SignIn).setOnClickListener {
             val name = view.findViewById<TextInputEditText>(R.id.nameField).text.toString()
             val courseName = view.findViewById<TextInputEditText>(R.id.nameField2).text.toString()
-            mUserViewModel.insert(UserEntity(name = name,courseName=courseName))
-            Navigation.findNavController(view).navigate(R.id.action_signUp_to_homePageFragment)
+
+            if (name.isEmpty() || courseName.isEmpty()) {
+                Toast.makeText(context,"Text Field Empty",Toast.LENGTH_SHORT).show()
+            } else {
+                mUserViewModel.insert(UserEntity(name = name, courseName = courseName))
+                Navigation.findNavController(view).navigate(R.id.action_signUp_to_homePageFragment)
+            }
         }
 
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner){
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
             activity?.finish()
         }
         return view
